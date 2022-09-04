0f7864ee01a5afa2d04bb7cdaaed3c8dd0de3724
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-04 11:00:34 +0900

    Add 267

--- abc267/d/main.rb
+++ abc267/d/main.rb
@@ -1,10 +1,13 @@
 N, M = gets.chomp.split.map(&:to_i)
-A = gets.chomp.split.map(&:to_i).sort
+A = gets.chomp.split.map(&:to_i).unshift(0)
 
-max = 0
+# 前からi個見てj個選んだ時の最大値
+dp = Array.new(N + 1) { Array.new(M, -10**15).unshift(0) }
 
-M.times do |i|
-  max += A[N - i - 1] * (M - i)
+1.upto(N) do |i|
+  1.upto(M) do |j|
+    dp[i][j] = i < j ? -10**15 : [dp[i - 1][j], dp[i - 1][j - 1] + j * A[i]].max
+  end
 end
 
-p max
+puts dp.last.last
