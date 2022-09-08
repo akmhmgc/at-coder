67e939d72c13b4e68bb7aad67e967933080eb650
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-08 22:36:23 +0900

    c_main

--- abc249/c/main.rb
+++ abc249/c/main.rb
@@ -1,29 +1,24 @@
-N ,K = gets.chomp.split.map(&:to_i)
+N, K = gets.chomp.split.map(&:to_i)
 
 S = []
-N.times do 
-  chs = gets.chomp.chars
-  hash = Hash.new(0)
-  chs.each{|c| hash[c] = 1}
-  S << hash  
+N.times do
+  s = Array.new(26, 0)
+  gets.chomp.chars.each { |ch| s[ch.ord - 97] = 1 }
+  S << s
 end
 
-p S
+max = 0
 
-# i個数選択してOKかどうか
-def ok(i)
-  flag = false
-  (0..15).to_a.combination(i).each do |com|
-    com.each do |i|
-      
-    end
+(2**N).times do |num|
+  moji = Array.new(26, 0)
+  N.times do |i|
+    # flag: i番目の値を選択するかどうか
+    flag = num >> i & 1 == 1
+    next unless flag
+
+    moji = [moji, S[i]].transpose.map { |a| a.inject(:+) }
   end
+  max = [moji.count(K), max].max
 end
 
-
-ok(2)
- 
-(2**K).times do |i|
-  i.to_s(2).rjust(K, '0').split('').map(&:to_i)
-end
-
+puts max
