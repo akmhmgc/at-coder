7c0eb6d8ad7f7eb7bfe0187b5dfe1ea73d42c45e
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-11 16:54:41 +0900

    再帰フィボナッチ

---
+++ algo-method/425/main.rb
@@ -0,0 +1,16 @@
+N = gets.to_i
+
+N = 20
+
+@memo = Array.new(N + 1, -1)
+@memo[0] = 0
+@memo[1] = 1
+
+def solve(x)
+  return @memo[x] if @memo[x] != -1
+
+  # 代入した値が帰り値となる
+  @memo[x] = solve(x - 1) + solve(x - 2)
+end
+
+puts solve(20)
