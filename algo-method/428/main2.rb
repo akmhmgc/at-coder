2814b5ed8ce7aab38acfdca6a57fe828e749833d
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-12 09:55:39 +0900

    同次式の計算

---
+++ algo-method/428/main2.rb
@@ -0,0 +1,17 @@
+# n個の球をm個に分ける方法の組み合わせ
+
+def dfs(n, m)
+  return [Array.new(m, 0)] if n == 0
+  return [[n]] if m == 1
+
+  ans = []
+  dfs(n, m - 1).each do |res|
+    res.unshift(0)
+    ans << res
+  end
+  dfs(n - 1, m).each do |res|
+    res[0] += 1
+    ans << res
+  end
+  ans
+end
