bf278222511d37400e2207a3402593838fbcbc98
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-18 10:02:16 +0900

    956

---
+++ algo-method/956/main.rb
@@ -0,0 +1,30 @@
+N,M = gets.chomp.split.map(&:to_i)
+G = Array.new(N){[]}
+
+M.times do 
+  a,b = gets.chomp.split.map(&:to_i)
+  G[a] << b
+  G[b] << a
+end
+
+@been = Array.new(N,false)
+@nodes = 0
+
+
+def rec(v)
+  @been[v] = true
+
+  G[v].each do |v_next| # n_vの方が変換でミスらないかも
+    next if @been[v_next] ==true
+
+    rec(v_next)
+  end
+end
+
+while !(v = @been.find_index(false)).nil?
+  @nodes += 1
+  rec(v)
+end
+
+puts @nodes
+
