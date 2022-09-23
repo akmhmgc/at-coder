bcc54ddf2438560c17a548daecca5107296ba617
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-23 15:33:27 +0900

    24　ALDS_11_B - 深さ優先探索]

---
+++ ALDS1_11/b/main.rb
@@ -0,0 +1,42 @@
+N = gets.to_i
+
+G = Array.new(N + 1) { [] }
+
+N.times do
+  u, k, *v = gets.chomp.split.map(&:to_i)
+
+  k.times do |i|
+    G[u] << v[i]
+  end
+end
+
+
+@been = Array.new(N + 1, false)
+@hakken = Array.new(N + 1, 0)
+@kanryou = Array.new(N + 1, 0)
+@time = 0
+
+def dfs(v)
+  return if @been[v] == true
+
+  @time += 1
+  @been[v] = true
+  @hakken[v] = @time
+
+  G[v].sort.each do |v_next|
+    dfs(v_next)
+  end
+
+  @time += 1
+  @kanryou[v] = @time
+end
+
+1.upto(N) do |i|
+  dfs(i)
+end
+
+p @been
+
+1.upto(N) do |i|
+  puts [i, @hakken[i], @kanryou[i]].join(' ')
+end
