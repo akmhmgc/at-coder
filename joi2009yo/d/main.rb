83ee6621eb35bbefea35fe7e9f5713715a172118
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-24 11:40:47 +0900

    joid

---
+++ joi2009yo/d/main.rb
@@ -0,0 +1,41 @@
+W = gets.to_i
+H = gets.to_i
+G = []
+
+H.times do
+  G << gets.chomp.split.map(&:to_i).map(&:zero?)
+end
+
+VEC = [
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+  [0, 1],
+]
+
+def dfs(x, y, been, count)
+  return if x < 0 || y < 0 || x >= H || y >= W || been[x][y] == true
+
+  been[x][y] = true
+  count += 1
+
+  VEC.each do |d_x, d_y|
+    x_next = x + d_x
+    y_next = y + d_y
+    dfs(x_next, y_next, been, count)    
+  end
+  been[x][y] = false
+
+  @max = [count, @max].max
+end
+
+@max = 0
+
+H.times do |i|
+  W.times do |j|
+    been = Marshal.load(Marshal.dump(G))
+    dfs(i, j, been, 0)
+  end
+end
+
+puts @max
