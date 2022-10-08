845ce511051f2c37bb93b670971afe65218146dc
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-08 12:32:24 +0900

    Q3. s-t パス (B) BFSで前に通った経路を保存しておく方法

---
+++ algo-method/968/main3.rb
@@ -0,0 +1,35 @@
+N, M, S, T = gets.chomp.split.map(&:to_i)
+G = Array.new(N) { [] }
+
+M.times do
+  a, b = gets.chomp.split.map(&:to_i)
+  G[a] << b
+end
+
+been = Array.new(N, false)
+prev = Array.new(N, -1)
+
+queue = [S]
+
+until (v = queue.shift).nil?
+  been[v] = true
+
+  G[v].each do |v_next|
+    next if been[v_next] == true
+
+    prev[v_next] = v
+    queue << v_next
+  end
+end
+
+
+order = []
+now = T
+while now != -1
+  order.append(now)
+  now = prev[now]
+end
+
+puts order.count
+puts order.reverse * ' '
+
