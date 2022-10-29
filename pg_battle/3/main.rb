7610b9f9584308c75d1021f6327633480a227877
akmhmgc <akimuhamaguchi@icloud.com>
2022-10-29 14:48:01 +0900

    pg-battle3

--- pg_battle/3/main.rb
+++ pg_battle/3/main.rb
@@ -1,2 +1,21 @@
 N = gets.to_i
 A = gets.chomp.split.map(&:to_i)
+
+seq = []
+
+count = 0
+(N - 1).downto(0) do |i|
+  ai = A[i]
+  count += ai
+  seq.unshift(ai)
+  if ai == 1
+    1.upto(N) do |j|
+      break unless seq.first == j
+
+      count -=  seq.shift
+    end
+  end
+end
+
+puts count
+
